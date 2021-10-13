ccd963594132e685db883b587ee2410caa3be614
(none) <john@iamjohnhenry.com>
2021-10-13 09:24:00 -0700

    build to 'dist' branch

--- public/index/js/index.mjs
+++ public/index/js/index.mjs
@@ -1,3 +1,7 @@
-import "https://johnhenry.github.io/std/js/css-variables-window@0.0.0/index.mjs";
-import "https://johnhenry.github.io/std/js/css-variables-mouse@0.0.0/index.mjs";
-import "https://johnhenry.github.io/std/js/css-variables-scroll@0.0.0/index.mjs";
+import "https://johnhenry.github.io/std/js/css-model-window@0.0.0/global.mjs";
+
+import "https://johnhenry.github.io/std/js/css-model-scroll@0.0.0/global.mjs";
+
+import "https://johnhenry.github.io/std/js/css-model-mouse@0.0.0/global.mjs";
+
+import "https://johnhenry.github.io/std/js/css-model-keyboard@0.0.0/global.mjs";
---
+++ src/SETTINGS.mjs
@@ -0,0 +1,2 @@
+export const SITE_TITLE = process.env.SITE_TITLE;
+export const TAG_MANAGER_ID = process.env.TAG_MANAGER_ID;
