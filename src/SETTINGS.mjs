c421a65ea402a3c6e26685d1686b2a7c23981229
(none) <john@iamjohnhenry.com>
2021-10-18 12:41:40 -0700

    add preliminary SEO

--- src/SETTINGS.mjs
+++ src/SETTINGS.mjs
@@ -1,4 +1,7 @@
+export const SITE_AUTHOR = "John Henry";
 export const SITE_TITLE = process.env.SITE_TITLE || "John Henry";
+export const SITE_CANONICAL_URL =
+  process.env.SITE_CANONICAL_URL || "https://iamjohnhenry.com";
 export const SITE_DESCRIPTION =
   process.env.SITE_DESCRIPTION || "John Henry's Person Portfolio and Blog";
 export const SITE_KEYWORDS = process.env.SITE_KEYWORDS
