da90c2e7e39e20ec1d78c1eef5e166e07f3a7361
(none) <john@iamjohnhenry.com>
2021-10-18 01:47:36 -0700

    update performance

--- src/SETTINGS.mjs
+++ src/SETTINGS.mjs
@@ -1,2 +1,7 @@
 export const SITE_TITLE = process.env.SITE_TITLE || "John Henry";
+export const SITE_DESCRIPTION =
+  process.env.SITE_DESCRIPTION || "John Henry's Person Portfolio and Blog";
+export const SITE_KEYWORDS = process.env.SITE_KEYWORDS
+  ? process.env.SITE_KEYWORDS.split(",")
+  : [];
 export const TAG_MANAGER_ID = process.env.TAG_MANAGER_ID || "";
