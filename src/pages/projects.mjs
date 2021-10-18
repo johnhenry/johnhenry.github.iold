7ca225ba8fe6689970b9c95189a74f859b619338
(none) <john@iamjohnhenry.com>
2021-10-18 11:36:34 -0700

    various updates

---
+++ src/pages/projects.mjs
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "Bash Command Line tools",
+    description: "A collection of tools I i find useful.",
+    href: "https://johnhenry.github.io/lib/bash",
+  },
+  {
+    title: "Standard Library -- set of components for building applications.",
+    description: "Click here",
+    href: "https://johnhenry.github.io/lib",
+    image:
+      "https://johnhenry.github.io/vendor/img/www.pexels.com/pixabay/turned-on-computer-monitor-displaying-text-300.jpg",
+  },
+  {
+    title: "Forsnaken -- Multiplayer snake game",
+    description: "A modular snake game",
+    href: "https://johnhenry.github.io/forsnaken",
+  },
+  {
+    title: "HTML Builder",
+    description: "Build HTML with components",
+    href: "https://johnhenry.github.io/htmlbuilder",
+  },
+  {
+    title: "Blog",
+    description: "Thoughts and musings",
+    href: "https://johnhenry.github.io/blog",
+  },
+];
