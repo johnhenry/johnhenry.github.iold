5f1c34615d37727cc4ac16281dae2c0be5bad383
(none) <john@iamjohnhenry.com>
2021-10-21 17:52:55 -0700

    add scaffolding for tags

--- src/utils/sort-posts.mjs
+++ src/utils/sort-posts.mjs
@@ -2,17 +2,24 @@ export default (posts) => {
   const sorted = posts
     .filter((b) => b.date)
     .sort((a, b) => new Date(b.date) - new Date(a.date));
-  const tagSet = new Set();
-  for (const { tags } of sorted) {
-    for (const tag of tags) {
-      tagSet.add(tag);
+  const tags = new Set();
+  const byTag = {};
+  for (const post of sorted) {
+    for (const tag of post.tags) {
+      tags.add(tag);
+      if (byTag[tag]) {
+        byTag[tag].push(post);
+      } else {
+        byTag[tag] = [post];
+      }
     }
   }
   const latest = sorted[0];
   return {
+    byTag: Object.entries(byTag),
     posts,
     sorted,
     latest,
-    tags: [...tagSet],
+    tags: [...tags],
   };
 };
